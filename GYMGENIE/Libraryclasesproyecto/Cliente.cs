d167234b24818efaf5512c5e43b803f8d2783404
JoaquinSalera <131197273+JoaquinSalera@users.noreply.github.com>
2023-06-05 20:19:11 -0300

    1comit

---
+++ GYMGENIE/Libraryclasesproyecto/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraryclasesproyecto
+{
+    public class Cliente : Persona
+    {
+        public int idClient { get; set; }
+    }
+}
---
+++ GYMGENIE/Libreria de clases/Clase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria_de_clases
+{
+    public class Clase
+    {
+        public int idClase { get; set; }
+        public string Nombreclase { get; set; }
+        public DateTime Horario { get; set; }
+    }
+}
