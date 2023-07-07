157eda7280b9aa9b1ffb39f0b731ff59f076fd92
CaceresFranco <131301873+CaceresFranco@users.noreply.github.com>
2023-07-07 18:35:35 -0300

    Entrega 3 (ABMs en back)
    Se revierte a el commit anterior, dado que por algun motivo se borraron las gestiones anteriores con los abm.
    112d51e522e21d00348cf9baf7cac2f83e909347.

---
+++ GYMGENIE/Libraryclasesproyecto/Clase.cs
@@ -0,0 +1,9 @@
+﻿namespace Libraryclasesproyecto
+{
+    public class Clase
+    {
+        public int idClase { get; set; }
+        public string Nombreclase { get; set; }
+        public DateTime Horario { get; set; }
+    }
+}
---
+++ GYMGENIE/Libraryclasesproyecto/Principal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraryclasesproyecto
+{
+    public class Principal
+    {
+        List<Cliente> ListaCliente = new List<Cliente>();
+        List<Profesor> ListaProfesor = new List<Profesor>();
+        List<Clase> ListaClase = new List<Clase>();
+        List<Administrador> ListaAdmin = new List<Administrador>();
+
+        public void Altacliente(int DNI, string nombre, string apellido, DateTime FechaNac) //es void porque no retorna ningun valor
+        {
+            Cliente clientenuevo = new Cliente();
+            clientenuevo.idClient = clientenuevo.idClient + 1;
+            clientenuevo.DNI = DNI;
+            clientenuevo.Nombre = nombre;
+            clientenuevo.Apellido = apellido;
+            clientenuevo.FechaNacimiento = FechaNac;
+            ListaCliente.Add(clientenuevo);
+        }
+        public void AltaProfesor(int DNI, string nombre, string apellido, DateTime FechaNac, string Mail, string Direccion)
+        {
+            Profesor profesornuevo = new Profesor();
+            profesornuevo.idprof = profesornuevo.idprof + 1;
+            profesornuevo.DNI = DNI;
+            profesornuevo.Nombre = nombre;
+            profesornuevo.Apellido = apellido;
+            profesornuevo.FechaNacimiento = FechaNac;
+            profesornuevo.Direccion = Direccion;
+            profesornuevo.Mail = Mail;
+            ListaProfesor.Add(profesornuevo);
+
+        }
+        public void AltaClase(string Nombreclase, DateTime Horario, int Nivelclase)
+        {
+            Clase clasenueva = new Clase();
+            clasenueva.idClase = clasenueva.idClase + 1;
+            clasenueva.Nombreclase = Nombreclase;
+            clasenueva.Horario = Horario;
+            ListaClase.Add(clasenueva);
+
+        }
+        public void AltaAdmin(int DNI, string nombre, string apellido, DateTime FechaNac, string Mail, string Direccion, string Telefono)
+        {
+            Administrador AdminNuevo = new Administrador();
+            AdminNuevo.idAdmin = AdminNuevo.idAdmin + 1;
+            AdminNuevo.DNI = DNI;
+            AdminNuevo.Nombre = nombre;
+            AdminNuevo.Apellido = apellido;
+            AdminNuevo.Mail = Mail;
+            AdminNuevo.Telefono = Telefono;
+            AdminNuevo.FechaNacimiento = FechaNac;
+            AdminNuevo.Direccion = Direccion;
+            ListaAdmin.Add(AdminNuevo);
+
+        }
+    }
+}
